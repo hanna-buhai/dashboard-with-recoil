bc2083401be55f97fd507e70bb9d7b234adbfb51
hannahbugai <hannah97bugai@gmail.com>
2021-07-08 17:36:01 +0300

    add lessons header

--- src/components/lessons/index.js
+++ src/components/lessons/index.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import DashboardCard from '../common/dashboardCard'
+import SectionHeader from '../common/sectionHeader'
+
+import './index.css'
 
 const Lessons = props => {
   return (
-
-      <DashboardCard>
+    <DashboardCard>
+      <div className="lessons">
+        <SectionHeader
+          header="Lessons"
+          actionLinkName="View All"
+          actionLink={() => console.log('View All in Lessons')}
+        />
         <p>Lessons</p>
-      </DashboardCard>
-   
+        <p>Lessons</p>
+        <p>Lessons</p>
+      </div>
+    </DashboardCard>
   )
 }
 
