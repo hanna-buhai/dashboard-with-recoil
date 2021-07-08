ccd05748f8a1829e93435cb44514b31d76c75b90
hannahbugai <hannah97bugai@gmail.com>
2021-07-08 16:43:15 +0300

    add Progressbar component

--- src/components/myStudents/student.js
+++ src/components/myStudents/student.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { useRecoilState, useRecoilValue } from 'recoil'
 import { studentsItemStateWithId } from '../../state/atoms'
 import { studentProgressStatsWithId } from '../../state/selectors'
+import Progressbar from '../common/progressbar'
 
 import './index.css'
 
@@ -16,7 +17,9 @@ const Student = ({ studentId }) => {
     <div className="my-student-info">
       <div className={`my-student-info__avatar my-student-info__avatar--${student.colorMarker}`} />
       <span className="my-student-info__name">{student.name}</span>
-      <div className="my-student-info__progressbar">*****</div>
+      <div className="my-student-info__progressbar">
+        <Progressbar value={studentProgression} colorMarker={student.colorMarker} />
+      </div>
       <div className="my-student-info__progress-percentage-container ">
         <span className="my-student-info__progress-percentage">{studentProgression}%</span>
       </div>
