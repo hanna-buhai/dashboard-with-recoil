23904724dcef8c6802a88789b3075404593ce0e9
hannahbugai <hannah97bugai@gmail.com>
2021-07-20 13:16:14 +0300

    update shadows and padding in completed tasks

---
+++ src/components/common/risedCardItem/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './styles.css'
+
+const RisedCardItem = ({ children }) => {
+  return <div className="rised-card-item">{children}</div>
+}
+
+RisedCardItem.propTypes = {}
+
+export default RisedCardItem
--- src/components/completedTasks/task.js
+++ src/components/completedTasks/task.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { useRecoilValue } from 'recoil'
+import RisedCardItem from '../common/risedCardItem'
 import { studentsItemStateWithId, assignmentItemStateWithId } from '../../state/atoms'
 
 import './index.css'
@@ -11,20 +12,22 @@ const Task = ({ taskId }) => {
   const assignment = useRecoilValue(assignmentItemStateWithId(taskId))
   const student = useRecoilValue(studentsItemStateWithId(assignment?.assigneeId))
   if (!assignment) return null
-
+  console.log('Task', assignment)
   return (
-    <div className="task">
-      <div className="task__icon" />
-      <div className="task__info">
-        <div>
-          <span>{assignment?.name}</span>
-        </div>
-        <div>
-          <span>{student.name}</span>,{' '}
-          <span>{assignment?.finishedDate?.toLocaleDateString('en-US', options)}</span>
+    <RisedCardItem>
+      <div className="task">
+        <div className="task__icon" />
+        <div className="task__info">
+          <div>
+            <span>{assignment?.name}</span>
+          </div>
+          <div>
+            <span>{student.name}</span>,{' '}
+            <span>{assignment?.finishedDate?.toLocaleDateString('en-US', options)}</span>
+          </div>
         </div>
       </div>
-    </div>
+    </RisedCardItem>
   )
 }
 
